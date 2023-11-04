c80c911c534111ec1f0cef34cae9427f520b91e2
El2-D2-42 <fdvq38@durham.ac.uk>
2023-11-04 18:26:52 +0000

    Add Unity Project

--- API_Demo/Program.cs
+++ API_Demo/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using Newtonsoft.Json;
+using System.Net.Http;
 
 namespace ISSDeserializer
 {
     class Program
     {
-        static void Main(string[] args)
+        /*static void Main(string[] args)
         {
             string jsonResponse = @"{
                 ""name"": ""iss"",
@@ -29,23 +30,47 @@ namespace ISSDeserializer
             Console.WriteLine($"ID: {issData.id}");
             // ... (print other properties as needed)
             Console.ReadKey();
+        }*/
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            string url = "https://api.wheretheiss.at/v1/satellites/25544";
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    ISSData ?issData = JsonConvert.DeserializeObject<ISSData>(jsonResponse);
+
+                    Console.WriteLine($"Name: {issData.name}");
+                    Console.WriteLine($"Altitude: {issData.altitude}");
+                    Console.WriteLine($"Longitude: {issData.longitude}");
+                    Console.WriteLine($"Latitude: {issData.latitude}");
+                    // ... (print other properties as needed)
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {response.StatusCode}");
+                }
+            }
+            Console.ReadKey();
         }
     }
 
     public class ISSData
     {
-        public string name { get; set; }
+        public string ?name { get; set; }
         public int id { get; set; }
         public double latitude { get; set; }
         public double longitude { get; set; }
         public double altitude { get; set; }
         public double velocity { get; set; }
-        public string visibility { get; set; }
+        public string ?visibility { get; set; }
         public double footprint { get; set; }
         public long timestamp { get; set; }
         public double daynum { get; set; }
         public double solar_lat { get; set; }
         public double solar_lon { get; set; }
-        public string units { get; set; }
+        public string ?units { get; set; }
     }
 }
